49ba279cbfcaf2a2ca3f9f71ba0afa5bf8da9011
ssolovev <me@sergeysolovev.com>
2017-02-08 22:01:03 +0800

    Rename EmployeeLeaveController and insides

--- Controllers/EmployeeLeavesController.cs
+++ Controllers/EmployeeLeavesController.cs
@@ -5,34 +5,34 @@ using Microsoft.AspNetCore.Mvc;
 namespace ABC.Leaves.Api.Controllers
 {
     [Route("api/employee/leave")]
-    public class EmployeeLeaveController : Controller
+    public class EmployeeLeavesController : Controller
     {
-        private readonly IEmployeeLeavesService employeeLeavesService;
-        
-        public EmployeeLeaveController(IEmployeeLeavesService employeeLeavesService)
+        private readonly IEmployeeLeavesService service;
+
+        public EmployeeLeavesController(IEmployeeLeavesService service)
         {
-            this.employeeLeavesService = employeeLeavesService;
+            this.service = service;
         }
 
         [Route("apply")]
         [HttpPost]
         public IActionResult Apply([FromBody]EmployeeLeaveViewModel employeeLeaveViewModel)
         {
-            return employeeLeavesService.Apply(employeeLeaveViewModel);
+            return service.Apply(employeeLeaveViewModel);
         }
 
         [Route("{id}/approve")]
         [HttpPut]
         public IActionResult Approve(string id, [FromBody]string accessToken)
         {
-            return employeeLeavesService.Approve(id, accessToken);
+            return service.Approve(id, accessToken);
         }
 
         [Route("{id}/decline")]
         [HttpPut]
         public IActionResult Decline(string id, [FromBody]string accessToken)
         {
-            return employeeLeavesService.Decline(id, accessToken);
+            return service.Decline(id, accessToken);
         }
     }
 }
