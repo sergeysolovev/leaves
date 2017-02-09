c9e3e95b58b70e76025f802412d671a3b18f304c
ssolovev <me@sergeysolovev.com>
2017-02-10 02:00:07 +0800

    Add middleware for admin users authorization

---
+++ Middleware/AuthorizationMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ABC.Leaves.Api.Repositories;
+using Microsoft.AspNetCore.Http;
+using System.Net.Http;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using ABC.Leaves.Api.GoogleAuth;
+using ABC.Leaves.Api.GoogleAuth.Dto;
+using System.Net;
+
+namespace ABC.Leaves.Api.Middleware
+{
+    public class AuthorizationMiddleware
+    {
+        private readonly RequestDelegate next;
+        private readonly IEmployeeRepository employeeRepository;
+        private readonly IGoogleAuthService googleAuthService;
+
+        public AuthorizationMiddleware(RequestDelegate next,
+            IEmployeeRepository employeeRepository,
+            IGoogleAuthService googleAuthService)
+        {
+            this.next = next;
+            this.employeeRepository = employeeRepository;
+            this.googleAuthService = googleAuthService;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            string accessToken = GetOAuthAccessToken(context);
+            if (accessToken != null)
+            {
+                var input = new GetAccessTokenInfoInput { AccessToken = accessToken };
+                var output = await googleAuthService.GetAccessTokenInfoAsync(input);
+                if (output.Error != null)
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    return;
+                }
+                var userEmail = output.Email;
+                if (!employeeRepository.CheckUserIsAdmin(output.Email))
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                    return;
+                }
+                await next(context);
+            }
+        }
+
+        private static string GetOAuthAccessToken(HttpContext context)
+        {
+            string accessToken = null;
+            string bearerAuthHeader = context.Request
+                .Headers["Authorization"]
+                .FirstOrDefault(h => h.StartsWith("Bearer"));
+            if (bearerAuthHeader != null)
+            {
+                accessToken = bearerAuthHeader.Substring("Bearer ".Length).Trim();
+            }
+            return accessToken;
+        }
+    }
+}
---
+++ Middleware/AuthorizationPipeline.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace ABC.Leaves.Api.Middleware
+{
+    public class AuthorizationPipeline
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMiddleware<AuthorizationMiddleware>();
+        }
+    }
+}
