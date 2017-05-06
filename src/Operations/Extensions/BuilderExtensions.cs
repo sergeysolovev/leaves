7a5a2c0b79d9f12987483194dfc51be6b0f5993b
ssolovev <me@sergeysolovev.com>
2017-05-06 19:17:10 +0800

    Update TReturn -> TBuilder on builder extensions

--- src/Operations/Extensions/BuilderExtensions.cs
+++ src/Operations/Extensions/BuilderExtensions.cs
@@ -5,34 +5,34 @@ namespace Operations
 {
     public static class BuilderExtensions
     {
-        public static TReturn With<TResult, TReturn>(
-            this IBuilder<TResult, TReturn> source,
+        public static TBuilder With<TResult, TBuilder>(
+            this IBuilder<TResult, TBuilder> source,
             IOperationService<TResult> service)
-            where TReturn : IBuilder<TResult, TReturn>
+            where TBuilder : IBuilder<TResult, TBuilder>
             => source.Return(source.Build().Bind(service));
 
-        public static TReturn With<TResult, TReturn>(
-            this IBuilder<TResult, TReturn> source,
+        public static TBuilder With<TResult, TBuilder>(
+            this IBuilder<TResult, TBuilder> source,
             Func<TResult, Task<IContext<TResult>>> closure)
-            where TReturn : IBuilder<TResult, TReturn>
+            where TBuilder : IBuilder<TResult, TBuilder>
             => source.Return(source.Build().Bind(closure));
 
-        public static TReturn With<TResult, TReturn>(
-            this IBuilder<TResult, TReturn> source,
+        public static TBuilder With<TResult, TBuilder>(
+            this IBuilder<TResult, TBuilder> source,
             Func<TResult, IContext<TResult>> closure)
-            where TReturn : IBuilder<TResult, TReturn>
+            where TBuilder : IBuilder<TResult, TBuilder>
             => source.Return(source.Build().Bind(closure));
 
-        public static TReturn With<TResult, TReturn>(
-            this IBuilder<TResult, TReturn> source,
+        public static TBuilder With<TResult, TBuilder>(
+            this IBuilder<TResult, TBuilder> source,
             Func<TResult, TResult> selector)
-            where TReturn : IBuilder<TResult, TReturn>
+            where TBuilder : IBuilder<TResult, TBuilder>
             => source.Return(source.Build().Select(selector));
 
-        public static TReturn When<TResult, TReturn>(
-            this IBuilder<TResult, TReturn> source,
+        public static TBuilder When<TResult, TBuilder>(
+            this IBuilder<TResult, TBuilder> source,
             Func<TResult, bool> predicate)
-            where TReturn : IBuilder<TResult, TReturn>
+            where TBuilder : IBuilder<TResult, TBuilder>
             => source.Return(source.Build().Where(predicate));
     }
 }
