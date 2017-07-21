a9e7faf1c1a008b253bdb3a1249d2f62ea8ed5aa
ssolovev <me@sergeysolovev.com>
2017-07-22 00:38:04 +0800

    Add HomeController for the client

---
+++ src/clients/aspnetcore/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Leaves.Client.DataContracts;
+using Leaves.Client.Helpers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Leaves.Client.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => Redirect("/leaves");
+
+        [Route("idtoken")]
+        public IActionResult IdToken() => Redirect("/auth/idtoken");
+    }
+}
